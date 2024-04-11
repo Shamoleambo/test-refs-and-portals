dcb3b4b9fa18261b7baf5427cad6409065c7bf91
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-11 12:41:34 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH, I AM THE  S U P E R   P O R T A Lgit s AAAAAHHHgit s

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import SuperSimplePortal from './components/SuperSimplePortal'
 function App() {
   const [name, setName] = useState('')
   const nameInput = useRef()
+  const modal = useRef()
 
   function handleClick() {
     setName(nameInput.current.value)
+    modal.current.showModal()
   }
 
   return (
     <>
-      <SuperSimplePortal />
+      <SuperSimplePortal ref={modal} name={name} />
       <h1>The Super App</h1>
       <SimpleForm name={name} onButtonClick={handleClick} ref={nameInput} />
     </>
--- src/components/SuperSimplePortal.jsx
+++ src/components/SuperSimplePortal.jsx
@@ -1,11 +1,15 @@
-export default function SuperSimplePortal() {
+import { forwardRef } from 'react'
+
+const SuperSimpleForm = forwardRef(function ({ name }, ref) {
   return (
-    <dialog open>
-      <h3>Hello my friend, I am a Portal</h3>
+    <dialog ref={ref}>
+      <h3>Hello {name}, I am a Portal</h3>
       <p>how are you doing?</p>
       <form method='dialog'>
         <button>Bye Bye</button>
       </form>
     </dialog>
   )
-}
+})
+
+export default SuperSimpleForm
