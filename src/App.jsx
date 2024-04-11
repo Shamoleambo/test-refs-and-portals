f508d8e2fe0f05a8ee8f7b97283527c6af96814d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-11 12:27:05 -0300

    create the most beatiful portal that ever existed, is it possible to make it even better?

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import SimpleForm from './components/SimpleForm'
+import SuperSimplePortal from './components/SuperSimplePortal'
 
 function App() {
   return (
     <>
+      <SuperSimplePortal />
       <h1>The Super App</h1>
       <SimpleForm />
     </>
---
+++ src/components/SuperSimplePortal.jsx
@@ -0,0 +1,11 @@
+export default function SuperSimplePortal() {
+  return (
+    <dialog open>
+      <h3>Hello my friend, I am a Portal</h3>
+      <p>how are you doing?</p>
+      <form method='dialog'>
+        <button>Bye Bye</button>
+      </form>
+    </dialog>
+  )
+}
